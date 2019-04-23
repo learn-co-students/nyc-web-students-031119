4c6c311a643fc91f3dcf0412d8282900a0fdb3a6
Wangtron <evansjwang@gmail.com>
2019-04-23 11:57:40 -0400

    LECTURE POWER! 🙏

---
+++ 28-DOM-intro/src/index.js
@@ -0,0 +1,57 @@
+console.log('%c Hello, World!', 'color: purple')
+
+
+
+
+
+
+// need those pictures of memes
+  // given or upload yourself
+// page to put them on THIS IS THE HTML
+  // identify index.html
+// where on the page i want to add them
+  // div with the id="container"
+// control the element I want to add picture
+  // document.getElementById('container')
+    // store into a variable
+      const imgContainer = document.getElementById('container')
+// lookup how to add a image to an element
+  // create an img tag to add the images to
+    const imgTag = document.createElement('img')
+    // add the src for the img
+    imgTag.src = "source of the image"
+
+  // found appendChild
+    // lookedup how to use .appendChild
+      imgContainer.appendChild(imgTag)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rugrats netflix adaptation
+"https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/10676315_10203656694270611_6060927043296061113_n.jpg?_nc_cat=106&_nc_ht=scontent-lga3-1.xx&oh=c973e800cd0caf352a655aef6d180264&oe=5CC2BB63"
+
+
+// go to FAv site.. <3 manipulate something
